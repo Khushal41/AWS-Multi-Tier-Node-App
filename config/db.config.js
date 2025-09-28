898988cd0196c30c3ec0ca953c734c55294ba6b6
Khushal Bhavsar <khushalbhavsar41@gmail.com>
2025-09-28 17:22:59 +0530

    Commit

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
     HOST: process.env.DB_HOST || "database-1.cleya8eei4v2.eu-west-2.rds.amazonaws.com",
     USER: process.env.DB_USER || "admin",
     PASSWORD: process.env.DB_PASSWORD || "admin123",
-    DB: process.env.DB_NAME || "database- 1",
+    DB: process.env.DB_NAME || "database-1",
     dialect: "mysql"
 };
 
